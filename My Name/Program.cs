bcf8ab12e6e74636b5b9b2936fddc75439e8c14f
DarylMickoSevilla <mickosevilla@gmail.com>
2022-05-01 16:49:08 +0800

    Final Product

---
+++ My Name/Program.cs
@@ -0,0 +1,15 @@
+namespace My_Name
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string message = "Daryl Micko R. Sevilla";
+            MessageBox.Show(message);
+        }
+    }
+}
